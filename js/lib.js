92abff65ed79b1afe341795affa8d813567d8764
DianaFields <dian@iMac-de-Diana.local>
2017-06-04 22:38:14 -0500

    parallax

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,4 @@
+
 function loadEx(){
 	$("#header").load("sections/nav.html"); 
   	$("#footer").load("sections/footer.html"); 
@@ -6,230 +7,7 @@ function loadInter(){
 	$("#header").load("../sections/nav.html"); 
   	$("#footer").load("../sections/footer.html"); 
 }
-function galleryMain(){ 
-var app = angular.module("app", ["ngAnimate"]);
 
-app.controller("mainCtrl", function($scope) {
-	$scope.boxes = [
-		{
-			name: "Softermia S.A. de C.V.",
-			image: "images/galeria/instalacion-01.jpg"
-		},
-		{
-			name: "Softermia S.A. de C.V.",
-			image: "images/galeria/instalacion-02.jpg"
-		},
-		{
-			name: "Softermia S.A. de C.V.",
-			image: "images/galeria/instalacion-03.jpg"
-		},
-		{
-			name: "Softermia S.A. de C.V.",
-			image: "images/galeria/instalacion-04.jpg"
-		},
-		{
-			name: "Softermia S.A. de C.V.",
-			image: "images/galeria/instalacion-05.jpg"
-		},
-		{
-			name: "Softermia S.A. de C.V.",
-			image: "https://source.unsplash.com/Ixp4YhCKZkI/900x900"
-		},
-		
-	];
-
-	$scope.selected = [];
-	$scope.selectBox = function(item, position) {
-		$scope.selected = [
-			{
-				item: item,
-				position: position
-			}
-		];
-		$scope.$apply();
-	};
-	$scope.clearSelection = function() {
-		$scope.selected = [];
-	};
-});
-
-app.directive("box", function() {
-	return {
-		restrict: "E",
-		scope: {},
-		bindToController: {
-			onSelect: "=",
-			item: "="
-		},
-		controllerAs: "box",
-		controller: function() {
-			var box = this;
-
-			box.goFullscreen = function(e) {
-				box.onSelect(box.item, e.target.getBoundingClientRect());
-			};
-		},
-		link: function(scope, element) {
-			element.bind("click", scope.box.goFullscreen);
-			element.css({
-				"background-image": "url(" + scope.box.item.image + ")"
-			});
-		}
-	};
-});
-
-app.directive("bigBox", function($timeout) {
-	return {
-		restrict: "AE",
-		scope: {},
-		bindToController: {
-			position: "=",
-			selected: "=",
-			onSelect: "="
-		},
-		controllerAs: "box",
-		controller: function() {
-			var box = this;
-		},
-		link: function(scope, element) {
-			var css = {};
-			for (var key in scope.box.position) {
-				css[key] = scope.box.position[key] + "px";
-			}
-
-			element.css(css);
-
-			$timeout(function() {
-				element.css({
-					top: "0",
-					left: "0",
-					right: "0"
-				});
-				element.addClass("image-out");
-			}, 200);
-
-			$timeout(function() {
-				element.css({
-					width: "100%",
-					height: "100%"
-				});
-			}, 500);
-
-			$timeout(function() {
-				element.addClass("show");
-			}, 800);
-		}
-	};
-});
-
-
-}
-function galleryExt(){
-var app = angular.module("app", ["ngAnimate"]);
-
-app.controller("mainCtrl", function($scope) {
-	$scope.boxes = [
-		{
-			name: "Softermia S.A. de C.V.",
-			image: "../images/galeria/instalacion-01.jpg"
-		},
-		{
-			name: "Softermia S.A. de C.V.",
-			image: "../images/galeria/instalacion-02.jpg"
-		},
-		{
-			name: "Softermia S.A. de C.V.",
-			image: "../images/galeria/instalacion-03.jpg"
-		},
-		
-	];
-
-	$scope.selected = [];
-	$scope.selectBox = function(item, position) {
-		$scope.selected = [
-			{
-				item: item,
-				position: position
-			}
-		];
-		$scope.$apply();
-	};
-	$scope.clearSelection = function() {
-		$scope.selected = [];
-	};
-});
-
-app.directive("box", function() {
-	return {
-		restrict: "E",
-		scope: {},
-		bindToController: {
-			onSelect: "=",
-			item: "="
-		},
-		controllerAs: "box",
-		controller: function() {
-			var box = this;
-
-			box.goFullscreen = function(e) {
-				box.onSelect(box.item, e.target.getBoundingClientRect());
-			};
-		},
-		link: function(scope, element) {
-			element.bind("click", scope.box.goFullscreen);
-			element.css({
-				"background-image": "url(" + scope.box.item.image + ")"
-			});
-		}
-	};
-});
-
-app.directive("bigBox", function($timeout) {
-	return {
-		restrict: "AE",
-		scope: {},
-		bindToController: {
-			position: "=",
-			selected: "=",
-			onSelect: "="
-		},
-		controllerAs: "box",
-		controller: function() {
-			var box = this;
-		},
-		link: function(scope, element) {
-			var css = {};
-			for (var key in scope.box.position) {
-				css[key] = scope.box.position[key] + "px";
-			}
-
-			element.css(css);
-
-			$timeout(function() {
-				element.css({
-					top: "0",
-					left: "0",
-					right: "0"
-				});
-				element.addClass("image-out");
-			}, 200);
-
-			$timeout(function() {
-				element.css({
-					width: "100%",
-					height: "100%"
-				});
-			}, 500);
-
-			$timeout(function() {
-				element.addClass("show");
-			}, 800);
-		}
-	};
-});
-
-
-}
 function animateScroll(){
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -247,4 +25,44 @@ $("div.imagen")
  	// .velocity("scroll", { duration: 750, offset: 250 })
   // .velocity({ left: 150 }, 800, "easeOutQuad")
   // .velocity({ left: 0 }, 800, "easeInSine"); 
+}
+var change = true;
+function pgdAnimation(){
+	$(window).scroll(function(){
+		var firstScreen = $("#firstScreen").offset().top;
+		var secondScreen = $("#secondScreen").offset().top;
+		var thirdScreen = $("#thirdScreen").offset().top;
+		var lastScreen = $("#lastScreen").offset().top;
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop > firstScreen){
+			$('.pgd').css('position','fixed');
+			$('.pgd').css('top','6em');
+		}
+		if(scrollTop < firstScreen){
+			$('.pgd').css('top','0');
+			$('.pgd').css('position','inherit');
+			
+		}
+		if(scrollTop > secondScreen){
+			$('.screen-01').css('opacity','1');
+			$('.screen-02').css('opacity','0');
+		}
+		if(scrollTop < secondScreen){
+			$('.screen-01').css('opacity','0');
+		}
+		if(scrollTop > thirdScreen){
+			$('.screen-02').css('opacity','1');
+			$('.screen-01').css('opacity','0');
+		}
+		if(scrollTop > lastScreen){
+			$('.pgd').css('position','inherit');
+			$('.pgd').css('margin-top','155em');
+			$('.screen-02').css('opacity','0');
+		}
+		if(scrollTop < lastScreen & scrollTop > firstScreen){
+			$('.pgd').css('position','fixed');
+			$('.pgd').css('margin-top','0');
+		}
+	});
+	 
 }
