52dffd8a44342024c7c1bf69882b42a83494435b
System Administrator <root@di.local>
2017-07-05 17:34:44 -0500

    update

--- js/lib.js
+++ js/lib.js
@@ -1,25 +1,37 @@
 
+	var animationLoad;
+	function init() {
+		animationLoad = setTimeout(showPage, 2500);
+	}
+
+	function showPage() {
+		document.getElementById("load").style.display = "none";
+		document.getElementById("main-content").style.opacity = "1";
+	}
+
+
+
 function loadEx(){
 	$("#header").load("sections/nav.html"); 
-  	$("#footer").load("sections/footer.html"); 
+	$("#footer").load("sections/footer.html"); 
 }
 function loadInter(){
 	$("#header").load("../sections/nav.html"); 
-  	$("#footer").load("../sections/footer.html"); 
+	$("#footer").load("../sections/footer.html"); 
 }
 
 function animateScroll(){
 
-		(function($) {
-	    "use strict";
-	    $('a.page-scroll').bind('click', function(event) {
-		alert();
-	        var $ele = $(this);
-	        $('html, body').stop().animate({
-	            scrollTop: ($($ele.attr('href')).offset().top - 60)
-	        }, 1450, 'easeInOutExpo');
-	        event.preventDefault();
-	    });
+	(function($) {
+		"use strict";
+		$('a.page-scroll').bind('click', function(event) {
+			alert();
+			var $ele = $(this);
+			$('html, body').stop().animate({
+				scrollTop: ($($ele.attr('href')).offset().top - 60)
+			}, 1450, 'easeInOutExpo');
+			event.preventDefault();
+		});
 	})(jQuery);
 }
 
@@ -55,7 +67,7 @@ function pgdAnimation(){
 		// 	$('.screen-01').css('opacity','0');
 		// }
 		if(scrollTop > thirdScreen) {
-			 $('.screen-02').css('opacity','1');
+			$('.screen-02').css('opacity','1');
 			$('.screen-01').css('opacity','0');
 		}
 		// if(scrollTop < thirdScreen){
@@ -83,56 +95,56 @@ function pgdAnimation(){
 			$('.pgd').css('margin-top','0');
 		}
 	});
-	 $( ".mk-01" ).mouseover(function() {
-  		$(".mk-01-open").css("opacity","1");
+	$( ".mk-01" ).mouseover(function() {
+		$(".mk-01-open").css("opacity","1");
 	});
-	 $( ".mk-02" ).mouseover(function() {
-  		$(".mk-02-open").css("opacity","1");
+	$( ".mk-02" ).mouseover(function() {
+		$(".mk-02-open").css("opacity","1");
 	});
-	 $( ".mk-03" ).mouseover(function() {
-  		$(".mk-03-open").css("opacity","1");
+	$( ".mk-03" ).mouseover(function() {
+		$(".mk-03-open").css("opacity","1");
 	});
-	 $( ".mk-01" ).mouseleave(function() {
-  		$(".mk-01-open").css("opacity","0");
+	$( ".mk-01" ).mouseleave(function() {
+		$(".mk-01-open").css("opacity","0");
 	});
-	 $( ".mk-02" ).mouseleave(function() {
-  		$(".mk-02-open").css("opacity","0");
+	$( ".mk-02" ).mouseleave(function() {
+		$(".mk-02-open").css("opacity","0");
 	});
-	 $( ".mk-03" ).mouseleave(function() {
-  		$(".mk-03-open").css("opacity","0");
+	$( ".mk-03" ).mouseleave(function() {
+		$(".mk-03-open").css("opacity","0");
 	});
 
 }
 function backToTop(){
-var pxScrolled = 1000;
-  var duration = 500;
-  $(window).scroll(function() {
-  	
-    if ($(this).scrollTop() > pxScrolled) {
-      $('.backtop').css({'opacity': '1', 'transition': '.6s'});
-    } else {
-      $('.backtop').css({'opacity': '0'});
-    } 
-  });
+	var pxScrolled = 1000;
+	var duration = 500;
+	$(window).scroll(function() {
 
-  $('.backtop').click(function() {
-    $('body').animate({scrollTop: 0}, duration);
-  })
+		if ($(this).scrollTop() > pxScrolled) {
+			$('.backtop').css({'opacity': '1', 'transition': '.6s'});
+		} else {
+			$('.backtop').css({'opacity': '0'});
+		} 
+	});
+
+	$('.backtop').click(function() {
+		$('body').animate({scrollTop: 0}, duration);
+	})
 }
 
 function menuAnimated(){
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 200) {
-    	 $('nav').addClass("nav-collapse");
-     
-    } else {
-      $('nav').removeClass("nav-collapse");
-    } 
-  });
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 200) {
+			$('nav').addClass("nav-collapse");
 
-  $('.backtop').click(function() {
-    $('body').animate({scrollTop: 0}, duration);
-  })
+		} else {
+			$('nav').removeClass("nav-collapse");
+		} 
+	});
+
+	$('.backtop').click(function() {
+		$('body').animate({scrollTop: 0}, duration);
+	})
 }
 
 
